d5c2b451288951bb99ed8e50eda2c16cbc197ee5
Ivan Bezruk <ivanbezruk2022@gmail.com>
2025-05-25 00:43:12 +0300

    second_task_final

--- second_task.py
+++ second_task.py
@@ -11,3 +11,9 @@ def get_numbers_ticket(min, max, quantity):
     #Generate unique numbers and sort them
     numbers = random.sample(range(min, max + 1), quantity)
     return sorted(numbers)
+
+#Ask to enter numbers
+user_input = input("Enter min, max, quantity separated by commas: ")
+min_val, max_val, quantity = map(int, user_input.split(","))
+
+print(get_numbers_ticket(min_val, max_val, quantity))
