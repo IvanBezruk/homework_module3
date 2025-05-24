a12059ec76fa7b3459b706f2882438d3c343ed8d
Ivan Bezruk <ivanbezruk2022@gmail.com>
2025-05-24 23:57:20 +0300

    first_task

---
+++ #First task.py
@@ -0,0 +1,27 @@
+#First task
+import datetime
+
+#We create a functin that calculates how many days are between today's date and specific date
+def get_days_from_today(date):
+    #Convert the input string to a datetime oject
+    written_date = datetime.datetime.strptime(date, '%Y-%m-%d').date()
+
+    #Determination of taday's date
+    current_date = datetime.date.today()
+
+    #Calculate the difference in days
+    difference = current_date - written_date
+
+    #Return the difference in days
+    return difference.days
+
+#Show results as stated in the task
+print(get_days_from_today('2020-10-09'))
+
+#Ask user to enter a date
+user_input = input("Enter a date in YYYY-MM-DD format: ")
+
+#Show results per user input
+print(get_days_from_today(user_input))
+
+#Second task
